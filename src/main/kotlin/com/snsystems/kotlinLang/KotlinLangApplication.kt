d2e1a95273324fe90af5de04a79e19b3b69c76d7
sathyamus <sathyamus.sify@gmail.com>
2020-06-19 23:45:37 +0530

    Kotlin base

---
+++ src/main/kotlin/com/snsystems/kotlinLang/KotlinLangApplication.kt
@@ -0,0 +1,11 @@
+package com.snsystems.kotlinLang
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinLangApplication
+
+fun main1(args: Array<String>) {
+	runApplication<KotlinLangApplication>(*args)
+}
---
+++ src/test/kotlin/com/snsystems/kotlinLang/KotlinLangApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.snsystems.kotlinLang
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinLangApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
